1b930f105bc0ec30bedd86f4cc024dc1c1116fd6
DaveKnud <daveknud@outlook.com>
2023-06-12 23:10:17 +0200

    dinamic header nav bar with axios added

---
+++ src/components/MenuLinks.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "MenuLinks",
+    props: {
+        details: Object
+    }
+}
+</script>
+
+<template>
+    <div class="container-links">
+        <h4>{{ details.link }}</h4>
+
+    </div>
+    <div> <img :src="details.button" alt=""></div>
+</template>
+
+<style scoped lang="scss">
+h4 {
+    font-weight: 700;
+    text-align: center;
+}
+
+img {
+    width: 30px;
+    height: 30px;
+}
+</style>
+
+
