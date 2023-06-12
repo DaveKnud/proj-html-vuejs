f8df2c144063f677715dd61a9a7cc1dc1fc8a007
DaveKnud <daveknud@outlokk.com>
2023-06-12 16:25:29 +0200

    first commit

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "PageHeader",
+}
+</script>
+
+<template>
+  <header class="container">
+    <h1>Ciao Header</h1>
+  </header>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  font-weight: 700;
+  text-align: center;
+}
+</style>
+
+
---
+++ src/components/PageMain.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "PageMain",
+}
+</script>
+
+<template>
+    <header class="container">
+        <h1>Ciao Main</h1>
+    </header>
+</template>
+
+<style scoped lang="scss">
+h1 {
+    font-weight: 700;
+    text-align: center;
+}
+</style>
